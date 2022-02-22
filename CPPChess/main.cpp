4b78c02cfe793db7a60b9d7af9f413b2cb08da23
Tyler Phalen <tyler.phalen@protonmail.com>
2022-02-21 18:51:45 -0700

    Initial Commit

---
+++ CPPChess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPPChess
+//
+//  Created by Tyler Phalen on 2/21/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
